2aafcc96daf0762181860b9ab8668fa2a289a605
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-03-28 12:11:04 -0300

    Exercise solution

--- src/main/java/Acronym.java
+++ src/main/java/Acronym.java
@@ -1,11 +1,20 @@
+import java.util.Arrays;
+
 class Acronym {
 
+    private final String acronym;
+
     Acronym(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        var sb = new StringBuilder();
+        Arrays.stream(phrase.split("[ _-]")).forEach(word -> {
+            if (!word.isBlank()) sb.append(word.charAt(0));
+        });
+
+        acronym = sb.toString().toUpperCase();
     }
 
     String get() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return acronym;
     }
 
 }
